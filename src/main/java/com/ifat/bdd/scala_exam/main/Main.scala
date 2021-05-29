34c55498a347612668ab3d4185db7a66c3008916
itankel <ifat.tankel@gmail.com>
2021-05-29 23:48:24 +0300

    create flow

--- src/main/java/com/ifat/bdd/scala_exam/main/Main.scala
+++ src/main/java/com/ifat/bdd/scala_exam/main/Main.scala
@@ -1,9 +1,6 @@
 package com.ifat.bdd.scala_exam.main
 
 import com.ifat.bdd.scala_exam.config.Configuration
-import com.ifat.bdd.scala_exam.repository.{ClientsRepo, PersonsRepo}
-import com.ifat.bdd.scala_exam.service.{ClientsService, CollectService, FilterBy, PersonsService, RequestService}
-import com.ifat.bdd.scala_exam.validators.AgeEmailPhoneValidator
 
 
 
